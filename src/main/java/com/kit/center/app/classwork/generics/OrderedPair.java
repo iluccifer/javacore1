deb15f9760c1aca7a5e7cac9b2c947d370b7c1d1
iluccifer <viktor.kucherenko.v@gmail.com>
2017-07-05 21:27:25 +0300

    new lesson

---
+++ src/main/java/com/kit/center/app/classwork/generics/OrderedPair.java
@@ -0,0 +1,22 @@
+package com.kit.center.app.classwork.generics;
+
+
+public class OrderedPair<K, V> implements Pair {
+    private K key;
+    private V value;
+
+    public OrderedPair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/com/kit/center/app/classwork/generics/Pair.java
@@ -0,0 +1,8 @@
+package com.kit.center.app.classwork.generics;
+
+
+public interface Pair<K, V> {
+    public K getKey();
+
+    public V getValue();
+}
